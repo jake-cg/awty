a9d7737707351faa9128dd2578958e244b7eb91c
jake-cg <jakegeorge125@live.com>
2018-05-09 19:58:40 -0700

    Completed part 1

---
+++ app/src/main/java/edu/washington/jcg25/awty/MainActivity.kt
@@ -0,0 +1,59 @@
+package edu.washington.jcg25.awty
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val messageText = findViewById<EditText>(R.id.messageText)
+        val phoneNumberText = findViewById<EditText>(R.id.phoneText)
+        val frequencyText = findViewById<EditText>(R.id.frequencyText)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+
+        val manager : AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        startButton.setOnClickListener{
+
+            val intent = Intent(this, AlarmReceiver::class.java)
+            intent.putExtra("message", messageText.text.toString())
+
+            val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+            if(startButton.text == "Start"){
+
+                if(messageText.text.toString() != "" && phoneNumberText.text.toString().length == 10 && frequencyText.text.toString() != ""){
+                    startButton.text = "Stop"
+
+                    val frequency : Long = (frequencyText.text.toString().toInt() * 60 * 1000).toLong()
+
+                    manager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), frequency, pendingIntent)
+
+                }
+            } else {
+                manager.cancel(pendingIntent)
+                startButton.text = "Start"
+            }
+        }
+    }
+}
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context, intent!!.extras.getString("message"), Toast.LENGTH_SHORT).show()
+    }
+}
